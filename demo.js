6e043d668e8287793d3efb03b98afd31078daa0a
xhzdcyy <912575054@qq.com>
2018-10-17 20:22:52 +0800

    脚本

---
+++ demo.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+
+var dirName = process.argv[2]
+if (fs.existsSync(dirName)) {
+    console.log('error: dir exists')
+}
+else {
+    fs.mkdirSync("./" + dirName)
+    process.chdir("./" + dirName)
+    fs.mkdirSync('css')
+    fs.mkdirSync('js')
+
+    fs.writeFileSync("./index.html", `<!DOCTYPE>
+<title>Hello</title>
+<h1>Hi</h1>`)
+    fs.writeFileSync("css/style.css", "h1{color: red;}")
+    fs.writeFileSync("./js/main.js", `var string = "Hello World"
+alert(string)`)
+}
+
+process.exit(0)
+
